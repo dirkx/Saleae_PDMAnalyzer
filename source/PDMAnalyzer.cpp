6f28047a0f71c8700cb24eb73473c480032386e6
Dirk-Willem van Gulik <dirkx@webweaving.org>
2025-09-22 16:05:30 +0200

    Tweaks to make the output easier to understand & pick up the signal earlier

--- source/PDMAnalyzer.cpp
+++ source/PDMAnalyzer.cpp
@@ -36,46 +36,55 @@ void PDMAnalyzer::WorkerThread()
 	// Find the start of a low.
 	if( mClock->GetBitState() == BIT_HIGH ) {
 		mClock->AdvanceToNextEdge();
-	} else {
+	} 
+#if 0
+	else {
 		mClock->AdvanceToNextEdge();
 		mClock->AdvanceToNextEdge();
 	}
-
 	U64 low_start = mClock->GetSampleNumber();
 	U64 low_duration = 0;
 	U64 high_duration = 0;
 	mClock->AdvanceToNextEdge();
-	while (low_duration == 0 || abs(low_duration - high_duration) > (low_duration + high_duration) / 2) {
+	while (low_duration == 0 || std::abs((long long)(low_duration - high_duration)) > (low_duration + high_duration) / 2) {
 		mClock->AdvanceToNextEdge(); // Falling edge
 		low_start = mClock->GetSampleNumber();
 		mClock->AdvanceToNextEdge(); // Rising edge
 		low_duration = mClock->GetSampleNumber() - low_start;
 		high_duration = mClock->GetSampleOfNextEdge() - mClock->GetSampleNumber();
 	}
-
+#endif
 
 	for( ; ; )
 	{
 		U8 data = 0;
 
 		U64 starting_sample = mClock->GetSampleNumber();
-
-		//let's put a dot exactly where we sample this bit:
-		mResults->AddMarker( mClock->GetSampleNumber(), AnalyzerResults::Dot, mSettings->mDataChannel );
+		mResults->AddMarker( mClock->GetSampleNumber(), AnalyzerResults::Dot, mSettings->mDataChannel);
 
 		for( U32 i=0; i< mSettings->mBitsPerSample; i++ )
 		{
+			mResults->AddMarker( mClock->GetSampleNumber(), 
+				(i) ? AnalyzerResults::DownArrow : AnalyzerResults::Dot, mSettings->mClockChannel);
+
 			mData->AdvanceToAbsPosition(mClock->GetSampleNumber());
 
-			if( mData->GetBitState() == BIT_HIGH )
-				data++;
+			bool isHigh = ( mData->GetBitState() == BIT_HIGH );
+			if (isHigh) data++;
 
+			U64 s = mClock->GetSampleNumber();
 			// Next falling edge.
 			// TODO(tannewt): Support stereo data by reading the data here.
 			mClock->AdvanceToNextEdge();
 
+			// Place the one in the middle of this low; alternative is getting it about
+			// 20-30 nanosconds after the down.
+			U64 e = mClock->GetSampleNumber();
+			mResults->AddMarker((s+e)/2, isHigh ? AnalyzerResults::One : AnalyzerResults::Zero, mSettings->mDataChannel );
+
 			// Next rising edge.
 			mClock->AdvanceToNextEdge();
+
 		}
 
 
